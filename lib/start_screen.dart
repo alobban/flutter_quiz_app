2b6d2e6d7e9f5f205ba6f805489dc537d9a1143a
Andrew Lobban <andrewglobban@gmail.com>
2024-01-26 12:50:56 -0600

    Apply full screen with Center widget

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -4,6 +4,8 @@ class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
   @override
   Widget build(context) {
-    return const Text('Start Screen');
+    return const Center(
+      child: Text('Start Screen'),
+    );
   }
 }
