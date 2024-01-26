534b6d7e9418b1529610d4dbaebdd42231a0bcc6
Andrew Lobban <andrewglobban@gmail.com>
2024-01-26 12:44:14 -0600

    Refactor with Container and add Gradient background

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,19 @@ import 'package:adv_basics/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
